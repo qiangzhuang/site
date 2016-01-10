f637e42492ce7af3321d37dca93e748386a48355
root <root@localhost.localdomain>
2016-01-09 23:08:41 -0800

    more changes

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ import os
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
+APP_POSTS = os.path.join(APP_ROOT, 'static', 'posts')
 
 app=Flask(__name__, static_path=None, static_url_path=None, static_folder='static', template_folder='templates', instance_path=None, instance_relative_config=False)
 flask_misaka.Misaka(app=app, renderer=None)
--- app/views.py
+++ app/views.py
@@ -1,24 +1,12 @@
 #import sqlite3
 from flask import Flask, request, g, redirect, url_for, render_template
 from flask_misaka import markdown
-from app import app, APP_ROOT, APP_STATIC
-import os
-
-
-def get_most_recent_file():
-    return os.path.join(APP_STATIC, "posts","post1.md")
-
+from app import app, APP_ROOT, APP_STATIC, APP_POSTS
+import os, datetime
 
 def gimme_data(f):
     with open(f) as fd:
         return fd.read()
-
-@app.route('/')
-def show_recent_post():
-    # get most recent markdown post in the folder of posts
-    
-    return render_template("markdown.html", post=gimme_data(get_most_recent_file()))
-
 
 @app.errorhandler(404)
 def not_found_error(error):
@@ -27,6 +15,32 @@ def not_found_error(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
+
+def list_dir(path):
+	d = os.listdir(path)
+	l = []
+	for item in d:
+		l.append(dict(name=item, modified=datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(path,item))).strftime('%Y-%m-%d %H:%M:%S')))
+	return l
+
+@app.route('/about')
+def about():
+	path = os.path.abspath(os.path.join(APP_STATIC, "about.md"))
+	return render_template("markdown.html", post=gimme_data(path))
+
+
+@app.route('/')
+@app.route('/<dir1>')
+@app.route('/<dir1>/')
+@app.route('/<dir1>/<second>')
+def temp(dir1='',second=''):
+	path = os.path.abspath(os.path.join(APP_POSTS,dir1, second))
+	if os.path.isdir(path) and path.startswith(APP_POSTS):
+	#	return list_dir(path)[0]['name']
+		return render_template("showdir.html", dircontents=list_dir(path),dir1=dir1, second=second)
+	elif os.path.isfile(path) and path.startswith(APP_POSTS):
+		return render_template("markdown.html", post=gimme_data(path), dir1=dir1, second=second)
+	return render_template('404.html'), 404
 
 #if __name__ == '__main__':
 #    app.run()
