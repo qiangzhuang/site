8f3368570374cdfdfc13527ddc923f31368f74a9
root <root@911911.pw>
2016-01-10 20:41:07 -0800

    adding update

--- startserver.fish
+++ startserver.fish
@@ -1,5 +1,5 @@
-#!/usr/local/bin/fish
+#!/usr/bin/fish
 #uwsgi --shared-socket 0.0.0.0:443 --https =0,ssl/foobar.crt,ssl/foobar.key,HIGH --master --processes 256 --gevent 1000 -w run:app --uid www-data --gid www-data --cheaper 4 --cheaper-initial 4 --cheaper-step 2 --check-static /root/flaskstuff/mysite/911911/app/static/ --offload-threads 4
 
-uwsgi -s /tmp/site.sock --master --processes 256 --gevent 1000 -w run:app --uid www-data --gid www-data --cheaper 4 --cheaper-initial 4 --cheaper-step 2 --check-static /root/flaskstuff/mysite/911911/app/static/ --offload-threads 4
+uwsgi -s /tmp/site.sock --master --processes 256 --gevent 1000 -w run:app --uid www-data --gid www-data --cheaper 4 --cheaper-initial 4 --cheaper-step 2 --check-static /www/site/911911/app/static/ --offload-threads 4 --daemonize /var/www/uwsgi.log
 
